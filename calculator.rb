3e5886fdd304408f2180df8fbe1bf7cb29fd348d
Erik McComb <erik@erikmccomb.com>
2017-08-16 14:54:24 -0600

    established general framework

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,50 @@
+# todo: build a calculator that supports all basic math operators + - * /
+# take the first number
+# take modifier
+# take the last number
+# puts result
+# error checking
+# user input and modifiers should be checked for proper input type
+# the result should set first_number and allow for another modifier and last number so result continues to build until the user decides to clear the calculator
+# - make + - * / work
+# - make clear work
+
+
+@first []
+@second []
+
+
 require 'pry'
 
-puts 'Welcome to the ruby calculator.'
+def calculator
+end
 
-# todo: build a calculator that supports all basic math operators + - * /
+def first_num
+  puts 'What is the first number?'
+  @calc << gets.to_i
+end
 
+def operator
+  puts 'Select an operator: +, -, *, /'
+  choice =gets.strip
+  case choice
+when '+'
+
+when '-'
+
+when '*'
+
+when '/'
+
+else
+  puts 'Invalid selection. Please choose a valid operator.'
+end
+
+def sec_num
+  puts 'What is the second number?'
+end
+
+def result
+  puts 'Calculating .......'
+  puts ''
+  puts "The result of "
